463e968749ff24d17c30ef4653629b463fd3efd8
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-12-23 14:04:58 +0100

    WordGenerator

--- app/shared/WordGenerator.jsx
+++ app/shared/WordGenerator.jsx
@@ -1,16 +1,22 @@
 "use client"
 
-import { useEffect } from "react"
+import { useState } from "react"
+
+const words = ["hello", "hi", "cat", "mouse", "car", "Paris", "candle", "step", "bed", "bird"]
 
 const WordGenerator = () => {
+  const [word, setWord] = useState("Nothing yet :-)");
 
-  useEffect(() => {
-    console.log("Hello world")
-  }, [])
+ const generate = () => {
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  setWord(randomWord);
+ }
 
   return (
     <div>
-      Hello world 
+      <button onClick={generate}>
+      Generate word</button>
+      <div>Generated: {word}</div>
     </div>
   )
 }
