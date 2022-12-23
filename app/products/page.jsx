160d9a58e1ba17087ca5b1bae8d593a02666f96e
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-12-23 15:37:46 +0100

    fetch static products + use

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,7 +1,23 @@
+import { use } from 'react';
+// https://blixtdev.com/all-about-reacts-new-use-hook/
+// https://vived.io/new-hook-is-coming-to-react-frontend-weekly-vol-109/
+
+async function getProducts() {
+
+  //getStaticProps
+  const res = await fetch("https://product-app-101-server.vercel.app/api/products");
+  return res.json();
+}
+
 const ProductsPage = () => {
+  const products = use(getProducts());
+
   return (
     <div className="page">
       <h1>Here're the products</h1>
+      <div>{ products.map(product =>
+        <div key={product.name}>{product.name}</div>
+        )}</div>
     </div>
   )
 }
