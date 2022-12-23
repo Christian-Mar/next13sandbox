fe78eebc74bbc25703b7f9fb76a5e6affaa0a7a2
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-12-23 16:06:40 +0100

    clientside fetch WSR - right URL

--- app/blogs/BlogsList.jsx
+++ app/blogs/BlogsList.jsx
@@ -13,21 +13,16 @@ import useSWR from 'swr';
 }
   */
 
-const fetcher = path =>
-	fetch(`https://product-app-101-server.vercel.app/api/products/${path}`).then(
-		res => res.json()
-	);
+const fetcher = (path) => fetch(`https://product-app-101-server.vercel.app/${path}`).then(res => res.json());
 
 const BlogList = () => {
 	const blogs = useSWR('api/products', fetcher);
 
 	return (
-		<div className='page'>
-			<div>
-				{blogs.data.map(product => (
-					<div key={blog.name}>{blog.name}</div>
-				))}
-			</div>
+		<div>
+			{blogs.data?.map(blog => (
+				<div key={blog.name}>{blog.name}</div>
+			))}
 		</div>
 	);
 };
