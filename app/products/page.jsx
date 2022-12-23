de56411b1ee861a60759ed491f96c2d06777d98d
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-12-23 16:35:40 +0100

    loading faster with suspense on product page

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,27 +1,13 @@
-import { use } from 'react';
-// https://blixtdev.com/all-about-reacts-new-use-hook/
-// https://vived.io/new-hook-is-coming-to-react-frontend-weekly-vol-109/
+import { Suspense } from 'react';
+import ProductList from './ProductList';
 
-async function getProducts() {
-	//getStaticProps - for serverSideProps add { cache: "no-store"}
-	const res = await fetch(
-		'https://product-app-101-server.vercel.app/api/products',
-		{ cache: 'no-store' }
+export default function ProductsPage() {
+	return (
+		<div className='page'>
+			<h1>Hi there Products Page</h1>
+			<Suspense fallback={<p>Loading Products</p>}>
+				<ProductList />
+			</Suspense>
+		</div>
 	);
-	return res.json();
 }
-
-const ProductsPage = () => {
-  const products = use(getProducts());
-
-  return (
-    <div className="page">
-      <h1>Here're the products</h1>
-      <div>{ products.map(product =>
-        <div key={product.name}>{product.name}</div>
-        )}</div>
-    </div>
-  )
-}
-
-export default ProductsPage;
